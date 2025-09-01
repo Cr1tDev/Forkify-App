98db13db26d0c3948b945bd05cce1f0574292f5b
Ivanarre <arre2397@gmail.com>
2025-09-01 20:34:55 +0800

    implemented search result

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,13 +1,20 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
+import { async } from 'regenerator-runtime';
+
+if (module.hot) {
+  module.hot.accept();
+}
 
 const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
+    console.log(id);
 
     if (!id) return;
     recipeView.renderSpinner();
@@ -25,16 +32,17 @@ const controlRecipes = async function () {
 
 const controlSearchResults = async function () {
   try {
+    resultsView.renderSpinner();
+
     // 1. Get search query
     const query = searchView.getQuery();
-
     if (!query) return;
 
     // 2. Load search
     await model.loadSearchResults(query);
 
     // 3. Render results
-    console.log(model.state.search.results);
+    resultsView.render(model.state.search.results);
   } catch (err) {
     console.log(err);
   }
